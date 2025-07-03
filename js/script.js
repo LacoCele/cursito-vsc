9399b4896fdc2beaa68aec4ec8fd294e5591ef7c
LacoCele <angelceori10@gmail.com>
2025-07-03 16:28:26 -0600

    jaja

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 let numA=20
 let numB=30
+let numC=40
 
-let res=numA+numB
-res++ 
-res++
-console.log(res+res)
+let res=numA+numB+numC
+let res2=res+res
+let res3=res/res2
+console.log(res+res2+res3)
